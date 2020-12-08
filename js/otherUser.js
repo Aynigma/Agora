1c67aebc878a0438e4155268c7fa4a07b0145e92
Whismirk <heolses@gmail.com>
2020-12-08 12:52:04 +0100

    Commiting suicide

--- js/otherUser.js
+++ js/otherUser.js
@@ -1,15 +1,20 @@
 var affect = 0;
+
 function change_follow_to_unfollow()
 {
     if (affect ==  0)
     {
         document.getElementById("button-follow").innerHTML = "Unfollow";
+        document.getElementById("modal").style.visibility = "visible";
         affect = 1;
     }
     else if(affect == 1)
     {
         document.getElementById("button-follow").innerHTML = "Follow";
+        document.getElementById("modal").style.visibility = "hidden";
         affect = 0;
     }
     
 }
+
+
