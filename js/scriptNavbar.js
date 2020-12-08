0a4671285be548fbc39b5596e910a407c9875d9d
Aynigma <49909849+Aynigma@users.noreply.github.com>
2020-12-08 13:41:02 +0100

    Add files via upload

--- js/scriptNavbar.js
+++ js/scriptNavbar.js
@@ -2,10 +2,17 @@ var x = document.getElementById("search-box-div");
 
 function showsearch() {
     var x = document.getElementById("search-box-div");
+
     x.style.display = "block";
 }
 
-function hidesearch() {
+
+function hide() {
     var x = document.getElementById("search-box-div");
     x.style.display = "none";
+}
+
+
+function hidesearch() {
+    setTimeout(hide, 1000);
 }
